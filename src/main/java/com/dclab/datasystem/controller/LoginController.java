0604623c02e720cd8a5c306ef55cb4df4fc7e2d7
Zhaohd <915525115@qq.com>
2019-03-05 10:58:38 +0800

    整合vue

--- src/main/java/com/dclab/datasystem/controller/LoginController.java
+++ src/main/java/com/dclab/datasystem/controller/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value="/api")
 public class LoginController {
     @Autowired
     private LoginService loginService;
